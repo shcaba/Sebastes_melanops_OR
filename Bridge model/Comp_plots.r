c46e91c8dcf3b5bd3d13e304dc0a570973218c28
Jason Cope <jason.cope@noaa.gov>
2022-11-04 15:55:18 -0700

    Add new plots for bridge comparison
    Includes SB and depletion.

--- Bridge model/Comp_plots.r
+++ Bridge model/Comp_plots.r
@@ -4,7 +4,14 @@ OR_comps<-read.csv("C:/Users/Jason.Cope/Desktop/Black rockfish 2022/Models/OR/Br
 OR_comps$SSversion<-as.factor(OR_comps$SSversion)
 ggplot(OR_comps,aes(Year,SB,group=SSversion,fill=SSversion))+
   geom_line(aes(color=SSversion,linetype=SSversion),lwd=1.25)+
-  ylim(0,2000)+
+  ylim(0,1700)+
   geom_ribbon(aes(ymin=low_SB,ymax=hi_SB),alpha=0.1)+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+ggplot(OR_comps,aes(Year,Depletion,group=SSversion,fill=SSversion))+
+  geom_line(aes(color=SSversion,linetype=SSversion),lwd=1.25)+
+  ylim(0,1)+
+  geom_ribbon(aes(ymin=low_dep,ymax=hi_dep),alpha=0.1)+
   theme_bw()+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
