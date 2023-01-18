24c0deae7803015c1056b5c01cb27925fcfc4a7b
shcaba <jason.cope@noaa>
2023-01-18 15:25:24 -0800

    Add landing page

---
+++ .history/docs/_render_script_20230117130728.R
@@ -0,0 +1,6 @@
+library(sa4ss)
+
+dir <- "C:/Assessments/2023/copper_rockfish_2023/docs"
+setwd(dir)
+
+rmarkdown::render("index.Rmd")
---
+++ .history/docs/_render_script_20230118151517.R
@@ -0,0 +1,6 @@
+library(sa4ss)
+
+dir <- "C:/Users/Jason.Cope/Documents/Github/Sebastes_melanops_OR/docs"
+setwd(dir)
+
+rmarkdown::render("index.Rmd")
